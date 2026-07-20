22b0d442e41078d47248574e4c0eb700637f73ee
cccccccoingjiang77 <cccccccoingjiang77@users.noreply.github.com>
2026-07-20 21:26:54 +0800

    copy: 精简平台活动Banner文案

--- site/data/activity.js
+++ site/data/activity.js
@@ -2,7 +2,7 @@ window.ACTIVITY_DATA = {
   enabled: true,
   title: "平台限时活动",
   image: "assets/activity/goose-life-festival.png",
-  text: "近期平台活动「鹅选美好生活节」正在火热进行中🔥，详情请戳👇",
+  text: "「鹅选美好生活节」正在火热进行中🔥，详情请戳👇",
   link: "https://doc.weixin.qq.com/doc/w3_AQkAEwaMADECNqzmTeL9RSAiQPLlR?scode=AJEAIQdfAAo6hKfAAhAQkAEwaMADE",
   updatedAt: "2026-07-20"
 };
