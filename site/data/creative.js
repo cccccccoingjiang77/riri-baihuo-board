c5dd4c289fb8f793a86498cd1931e18495d28701
cccccccoingjiang77 <hanwang0101@gmail.com>
2026-07-22 22:30:48 +0800

    frames: 更新「餐厨水具-餐具水具」第3条素材关键帧

--- site/data/creative.js
+++ site/data/creative.js
@@ -4014,9 +4014,30 @@ window.CREATIVE_DATA = {
           "ctr": 2.899562022,
           "cvr": 13.031234,
           "cpm": 262.7230787,
-          "duration": "未提供",
+          "duration": "约120s",
           "videoUrl": "http://adsmind.gdtimg.com/ads_svp_video__0b53c4afeaaayuabixb6knvbufyekilqausa.f0.mp4",
-          "frames": [],
+          "frames": [
+            {
+              "src": "assets/frames/upload-ebf4f87f-03-922d5e8850/frame-01.jpg",
+              "time": "7.2s"
+            },
+            {
+              "src": "assets/frames/upload-ebf4f87f-03-922d5e8850/frame-02.jpg",
+              "time": "28.8s"
+            },
+            {
+              "src": "assets/frames/upload-ebf4f87f-03-922d5e8850/frame-03.jpg",
+              "time": "51.5s"
+            },
+            {
+              "src": "assets/frames/upload-ebf4f87f-03-922d5e8850/frame-04.jpg",
+              "time": "74.3s"
+            },
+            {
+              "src": "assets/frames/upload-ebf4f87f-03-922d5e8850/frame-05.jpg",
+              "time": "98.2s"
+            }
+          ],
           "golden5": {
             "hook": {
               "time": "0-3S",
