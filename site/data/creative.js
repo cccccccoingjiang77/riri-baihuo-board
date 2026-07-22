b3613b3b0aa28b8503250b6f59f8fc954662bccd
cccccccoingjiang77 <hanwang0101@gmail.com>
2026-07-22 21:47:05 +0800

    frames: 更新「生活日用-其他」第7条素材关键帧

--- site/data/creative.js
+++ site/data/creative.js
@@ -4581,9 +4581,30 @@ window.CREATIVE_DATA = {
           "ctr": 2.45,
           "cvr": 7.4,
           "cpm": 65.68,
-          "duration": "未提供",
+          "duration": "约42s",
           "videoUrl": "http://adsmind.gdtimg.com/ads_svp_video__0bc3xecf4aaeyiajuebt4bvb5oiel24qixsa.f0.mp4",
-          "frames": [],
+          "frames": [
+            {
+              "src": "assets/frames/upload-fd0f5561-07-eb3d95efc5/frame-01.jpg",
+              "time": "2.5s"
+            },
+            {
+              "src": "assets/frames/upload-fd0f5561-07-eb3d95efc5/frame-02.jpg",
+              "time": "10.2s"
+            },
+            {
+              "src": "assets/frames/upload-fd0f5561-07-eb3d95efc5/frame-03.jpg",
+              "time": "18.2s"
+            },
+            {
+              "src": "assets/frames/upload-fd0f5561-07-eb3d95efc5/frame-04.jpg",
+              "time": "26.3s"
+            },
+            {
+              "src": "assets/frames/upload-fd0f5561-07-eb3d95efc5/frame-05.jpg",
+              "time": "34.7s"
+            }
+          ],
           "golden5": {
             "hook": {
               "time": "0-3S",
