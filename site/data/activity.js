d4752533ab826501bedf34c52575412d14615b78
cccccccoingjiang77 <cccccccoingjiang77@users.noreply.github.com>
2026-07-31 10:41:41 +0800

    feat: redesign campaign and activity banners

--- site/data/activity.js
+++ site/data/activity.js
@@ -1,8 +1,13 @@
 window.ACTIVITY_DATA = {
   enabled: true,
-  title: "平台限时活动",
-  image: "assets/activity/goose-life-festival.png",
-  text: "「鹅选美好生活节」正在火热进行中🔥，详情请戳👇",
-  link: "https://doc.weixin.qq.com/doc/w3_AQkAEwaMADECNqzmTeL9RSAiQPLlR?scode=AJEAIQdfAAo6hKfAAhAQkAEwaMADE",
-  updatedAt: "2026-07-20"
+  items: [
+    {
+      tag: "限时活动",
+      title: "鹅选美好生活节",
+      image: "assets/activity/goose-life-festival.png",
+      text: "正在火热进行中，点击查看活动详情",
+      link: "https://doc.weixin.qq.com/doc/w3_AQkAEwaMADECNqzmTeL9RSAiQPLlR?scode=AJEAIQdfAAo6hKfAAhAQkAEwaMADE",
+      updatedAt: "2026-07-20"
+    }
+  ]
 };
